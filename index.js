d466d1ac6ac0030301d522679f4b55dfe99de602
Md. Mahbubur Rahman <inspironkhl@gmail.com>
2023-06-11 09:50:40 +0600

    add dashboard

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 //into index.js file
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
-//env 
-require('dotenv').config()
+//env
+require("dotenv").config();
 
-
-//middle wares 
-app.use(cors())
-app.use(express.json())
-
-
-
+//middle wares
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8icusb.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,60 +19,60 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const teacherCollection = client.db('SpeakEase').collection('teacherDb')
-    const classesCollection = client.db('SpeakEase').collection('classesDb')
-    const cartCollection = client.db('SpeakEase').collection('cartDb')
+    const teacherCollection = client.db("SpeakEase").collection("teacherDb");
+    const classesCollection = client.db("SpeakEase").collection("classesDb");
+    const cartCollection = client.db("SpeakEase").collection("cartDb");
 
+    //get Teacher colloection
+    app.get("/teachers", async (req, res) => {
+      const cursor = teacherCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    //get classes collections
+    app.get("/allclasses", async (req, res) => {
+      const cursor = classesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // cart collection
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
 
-    //get Teacher colloection 
-    app.get('/teachers', async(req,res)=>{
-        const cursor = teacherCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get("/carts/:email", async (req, res) => {
+      const result = await cartCollection
+        .find({
+          email: req.params.email,
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
-
-
-    //get classes collections 
-    app.get("/allclasses", async(req, res)=>{
-        const cursor = classesCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
-
-
-
-// cart collection 
-app.post("/carts", async(req, res)=>{
-  const item = req.body
-  console.log(item)
-  const result = await cartCollection.insertOne(item)
-  res.send(result)
-})
-
-app.get("/carts/:email", async (req, res) => {
- 
-  const result = await cartCollection
-    .find({
-      email: req.params.email,
-    })
-    .toArray();
-  res.send(result);
-});
-
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -84,14 +80,10 @@ app.get("/carts/:email", async (req, res) => {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("speakease is running ");
+});
 
-
-
-
-app.get('/',(req,res)=>{
-    res.send('speakease is running ')
-})
-
-app.listen(port, ()=>{
-    console.log(`SpeakEase is runnig :${port}`);
-})
+app.listen(port, () => {
+  console.log(`SpeakEase is runnig :${port}`);
+});
