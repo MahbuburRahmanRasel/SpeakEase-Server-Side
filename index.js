6f1013ae81ba5ae33f2d8f403ffaddc23a7ac631
Md. Mahbubur Rahman <inspironkhl@gmail.com>
2023-06-11 22:15:49 +0600

    add payment 2

--- index.js
+++ index.js
@@ -4,12 +4,35 @@ const cors = require("cors");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+var jwt = require("jsonwebtoken");
+
 //env
 require("dotenv").config();
 
 //middle wares
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8icusb.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,6 +52,16 @@ async function run() {
     const teacherCollection = client.db("SpeakEase").collection("teacherDb");
     const classesCollection = client.db("SpeakEase").collection("classesDb");
     const cartCollection = client.db("SpeakEase").collection("cartDb");
+    const usersCollection = client.db("SpeakEase").collection("userDb");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     //get Teacher colloection
     app.get("/teachers", async (req, res) => {
@@ -45,28 +78,83 @@ async function run() {
     });
 
     // cart collection
-    app.post("/carts", async (req, res) => {
+    app.post("/carts",  async (req, res) => {
       const item = req.body;
-      console.log(item);
+
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
 
     app.get("/carts/:email", async (req, res) => {
+
+      const email = req.params.email
+
+      // const decodedEmail = req.decoded.email;
+      // if (email !== decodedEmail) {
+      //   return res.status(403).send({ error: true, message: 'forbidden access' })
+      // }
+
       const result = await cartCollection
         .find({
-          email: req.params.email,
+          email: email,
         })
         .toArray();
       res.send(result);
     });
 
-    app.delete('/carts/:id', async (req, res) => {
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
+
+    //user update
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
